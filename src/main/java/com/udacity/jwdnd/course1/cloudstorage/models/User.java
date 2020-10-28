16d1dce2891da7503f99a9a6f9d28eec2cb72c8b
Jonah Lou <jonahlou@gmail.com>
2020-10-28 12:50:52 -0400

    Initial commit

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/models/User.java
@@ -0,0 +1,35 @@
+package com.udacity.jwdnd.course1.cloudstorage.models;
+
+public class User {
+    private Integer userId;
+    private String username;
+    private String salt;
+    private String password;
+    private String firstName;
+    private String lastName;
+
+    public User(Integer userId, String username, String salt, String password, String firstName, String lastName) {
+        this.userId = userId;
+        this.username = username;
+        this.salt = salt;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/UserService.java
@@ -0,0 +1,28 @@
+package com.udacity.jwdnd.course1.cloudstorage.services;
+
+import com.udacity.jwdnd.course1.cloudstorage.mappers.UserMapper;
+import com.udacity.jwdnd.course1.cloudstorage.models.User;
+import org.springframework.stereotype.Service;
+
+import java.security.SecureRandom;
+import java.util.Base64;
+
+@Service
+public class UserService {
+    private HashService hashService;
+    private UserMapper userMapper;
+
+    public UserService(HashService hashService, UserMapper userMapper) {
+        this.hashService = hashService;
+        this.userMapper = userMapper;
+    }
+
+    public int createUser(User user) {
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+        String encodedSalt = Base64.getEncoder().encodeToString(salt);
+        String hashedPassword = hashService.getHashedValue(user.getPassword(), encodedSalt);
+        return userMapper.insert(new User(null, user.getUsername(), encodedSalt, hashedPassword, user.getFirstName(), user.getLastName()));
+    }
+}
