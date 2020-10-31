4407d3ce9bad26a890228e6ecb1689d73a22d9cd
Jonah Lou <jonahlou@gmail.com>
2020-10-31 14:22:13 -0400

    Minor styling changes

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/HomeController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/HomeController.java
@@ -16,28 +16,28 @@ import java.io.IOException;
 
 @Controller
 public class HomeController {
-    private final NoteService _noteService;
-    private final UserService _userService;
-    private final FileService _fileService;
-    private final CredentialService _credentialService;
-    private final EncryptionService _encryptService;
+    private final NoteService noteService;
+    private final UserService userService;
+    private final FileService fileService;
+    private final CredentialService credentialService;
+    private final EncryptionService encryptionService;
 
     private static final String NEW_NOTE = "newNote";
-    private static final String NEW_CRED = "newCred";
+    private static final String NEW_CREDENTIAL = "newCred";
     private static final String LIST_NOTES = "listNotes";
     private static final String LIST_FILES = "listFiles";
     private static final String LIST_CREDENTIALS = "listCredentials";
 
-    public HomeController(NoteService _noteService,
-                          UserService _userService,
-                          FileService _fileService,
-                          CredentialService _credentialService,
-                          EncryptionService _encryptService) {
-        this._noteService = _noteService;
-        this._userService = _userService;
-        this._fileService = _fileService;
-        this._credentialService = _credentialService;
-        this._encryptService = _encryptService;
+    public HomeController(NoteService noteService,
+                          UserService userService,
+                          FileService fileService,
+                          CredentialService credentialService,
+                          EncryptionService encryptionService) {
+        this.noteService = noteService;
+        this.userService = userService;
+        this.fileService = fileService;
+        this.credentialService = credentialService;
+        this.encryptionService = encryptionService;
     }
 
     @GetMapping("/home")
@@ -45,18 +45,18 @@ public class HomeController {
             @ModelAttribute(NEW_NOTE) Note newNote,
             Authentication auth,
             Model model) {
-        model.addAttribute(LIST_NOTES, _noteService.getNotes(auth.getName()));
-        model.addAttribute(LIST_FILES, _fileService.getFiles(auth.getName()));
-        model.addAttribute(LIST_CREDENTIALS, _credentialService.getCredentials(auth.getName()));
-        model.addAttribute("_encryptionService", _encryptService);
+        model.addAttribute(LIST_NOTES, noteService.getNotes(auth.getName()));
+        model.addAttribute(LIST_FILES, fileService.getFiles(auth.getName()));
+        model.addAttribute(LIST_CREDENTIALS, credentialService.getCredentials(auth.getName()));
+        model.addAttribute("_encryptionService", encryptionService);
         return "home";
     }
 
     @GetMapping("/result")
-    public String result(@RequestParam(required = false, name = "isSuccess") Boolean isSuccess,
+    public String result(@RequestParam(required = false, name = "success") Boolean success,
                          @RequestParam(required = false, name = "errorType") Integer errorType,
                          Model model) {
-        model.addAttribute("isSuccess", isSuccess);
+        model.addAttribute("success", success);
         model.addAttribute("errorType", errorType);
         return "result";
     }
@@ -65,13 +65,13 @@ public class HomeController {
     public String createUpdateNote(@ModelAttribute(NEW_NOTE) Note newNote,
                                    Authentication auth,
                                    Model model) {
-        Boolean result = _noteService.createUpdateNote(new Note(newNote.getNoteId(),
+        Boolean result = noteService.createUpdateNote(new Note(newNote.getNoteId(),
                 newNote.getNoteTitle(),
                 newNote.getNoteDescription(),
-                _userService.getUser(auth.getName()).getUserId()));
+                userService.getUser(auth.getName()).getUserId()));
 
-        model.addAttribute(LIST_NOTES, _noteService.getNotes(auth.getName()));
-        return "redirect:/result?isSuccess=" + result;
+        model.addAttribute(LIST_NOTES, noteService.getNotes(auth.getName()));
+        return "redirect:/result?success=" + result;
     }
 
     @GetMapping("/notes/delete/{noteId}")
@@ -79,9 +79,9 @@ public class HomeController {
                              @PathVariable Integer noteId,
                              Authentication auth,
                              Model model) {
-        Boolean result = _noteService.deleteNote(noteId);
-        model.addAttribute(LIST_NOTES, _noteService.getNotes(auth.getName()));
-        return "redirect:/result?isSuccess=" + result;
+        Boolean result = noteService.deleteNote(noteId);
+        model.addAttribute(LIST_NOTES, noteService.getNotes(auth.getName()));
+        return "redirect:/result?success=" + result;
     }
 
     @PostMapping(value = "/files/upload")
@@ -90,38 +90,38 @@ public class HomeController {
                              Authentication auth,
                              Model model) throws IOException {
         if(fileIn.isEmpty()) {
-            return "redirect:/result?isSuccess=" + false + "&errorType=1";
+            return "redirect:/result?success=" + false + "&errorType=1";
         }
 
-        if(_fileService.doesFileExist(fileIn.getOriginalFilename(), auth.getName())) {
-            return "redirect:/result?isSuccess=" + false + "&errorType=2";
+        if(fileService.doesFileExist(fileIn.getOriginalFilename(), auth.getName())) {
+            return "redirect:/result?success=" + false + "&errorType=2";
         }
-        Boolean result = _fileService.uploadFile(new File(
+        Boolean result = fileService.uploadFile(new File(
             null,
                 fileIn.getOriginalFilename(),
                 fileIn.getContentType(),
                 String.valueOf(fileIn.getSize()),
-                _userService.getUser(auth.getName()).getUserId(),
+                userService.getUser(auth.getName()).getUserId(),
                 fileIn.getBytes()));
 
-        model.addAttribute(LIST_FILES, _fileService.getFiles(auth.getName()));
-        return "redirect:/result?isSuccess=" + result;
+        model.addAttribute(LIST_FILES, fileService.getFiles(auth.getName()));
+        return "redirect:/result?success=" + result;
     }
 
     @GetMapping("/files/delete/{fileId}")
     public String deleteFile(@PathVariable Integer fileId,
                              Authentication auth,
                              Model model) {
-        Boolean result = _fileService.deleteFile(fileId);
-        model.addAttribute(LIST_FILES, _fileService.getFiles(auth.getName()));
-        return "redirect:/result?isSuccess=" + result;
+        Boolean result = fileService.deleteFile(fileId);
+        model.addAttribute(LIST_FILES, fileService.getFiles(auth.getName()));
+        return "redirect:/result?success=" + result;
     }
 
     @GetMapping("/files/download/{fileId}")
     public ResponseEntity download(@PathVariable Integer fileId,
                                    Authentication auth,
                                    Model model){
-        File selectedFile = _fileService.getFile(fileId);
+        File selectedFile = fileService.getFile(fileId);
 
         return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + selectedFile.getFilename() + "\"")
@@ -130,28 +130,28 @@ public class HomeController {
 
     @PostMapping(value = "/credentials/createUpdateCredential")
     public String createUpdateCredential(@ModelAttribute(NEW_NOTE) Note newNote,
-                                         @ModelAttribute(NEW_CRED)Credential newCred,
+                                         @ModelAttribute(NEW_CREDENTIAL)Credential newCred,
                                          Authentication auth,
                                          Model model){
-        Boolean result = _credentialService.createUpdateCredential(new Credential(
+        Boolean result = credentialService.createUpdateCredential(new Credential(
                         newCred.getCredentialId(),
                         newCred.getUrl(),
                         newCred.getUsername(),
                         newCred.getKey(),
                         newCred.getPassword(),
-                        _userService.getUser(auth.getName()).getUserId()));
+                        userService.getUser(auth.getName()).getUserId()));
 
-        model.addAttribute(LIST_CREDENTIALS, _credentialService.getCredentials(auth.getName()));
-        return "redirect:/result?isSuccess=" + result;
+        model.addAttribute(LIST_CREDENTIALS, credentialService.getCredentials(auth.getName()));
+        return "redirect:/result?success=" + result;
     }
 
     @GetMapping("/credentials/delete/{credId}")
     public String deleteCredential(@PathVariable Integer credId,
                              Authentication auth,
                              Model model) {
-        Boolean result = _credentialService.deleteCredential(credId);
-        model.addAttribute(LIST_NOTES, _credentialService.getCredentials(auth.getName()));
-        return "redirect:/result?isSuccess=" + result;
+        Boolean result = credentialService.deleteCredential(credId);
+        model.addAttribute(LIST_NOTES, credentialService.getCredentials(auth.getName()));
+        return "redirect:/result?success=" + result;
     }
 }
 
